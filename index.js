a60b9b3569170c2d8f2b329a31a9b84bfce96c03
gsingh01 <guriqbalsingh_hanjra@yahoo.com>
2022-05-15 13:39:51 -0700

    removes old file and deps update

--- index.js
+++
@@ -1,4 +0,0 @@
-export { default as LoginView } from './src/LoginView';
-export { default as adService } from './src/ADService';
-export { default as LogoutView } from './src/LogoutView';
-export { default as EditView } from './src/EditView';
